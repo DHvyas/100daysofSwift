9ba7448ce6797bfd28370769eda35dcc477063c5
DHvyas <vyasdhruvh@gmail.com>
2019-10-08 16:39:38 +0300

    day3

---
+++ Hello/Hello/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hello
+//
+//  Created by Dhruv Vyas on 08/10/19.
+//  Copyright © 2019 Dhruv Vyas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
