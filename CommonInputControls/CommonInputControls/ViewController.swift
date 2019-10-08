e3563fb76b71810d1bc82e0369d12c368eb9f616
DHvyas <vyasdhruvh@gmail.com>
2019-10-08 17:02:10 +0300

    day3

---
+++ CommonInputControls/CommonInputControls/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  CommonInputControls
+//
+//  Created by Dhruv Vyas on 08/10/19.
+//  Copyright © 2019 Dhruv Vyas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func buttonTapped(_ sender: Any) {
+        print("Button was tapped!")
+    }
+    
+
+}
+
